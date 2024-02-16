6f93d66bcaef48dd8a73b5528141ac6e5a3c9765
Nastya Gamza <nastyagamza2000@gmail.com>
2024-02-16 13:56:34 +0300

    refactor (sprint-1): add vars, replacing useWindowSize with useBreakpoint

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,7 +1,6 @@
 import {Link} from 'react-router-dom';
-import {useWindowSize} from 'usehooks-ts'
 import {useSidebarContext} from '../../context/sidebar/use-sidebar-context.ts';
-import {Button, Divider, Layout} from 'antd';
+import {Button, Divider, Grid, Layout} from 'antd';
 import {MenuList} from '@components/menu-list';
 import {MenuFoldOutlined, MenuUnfoldOutlined} from '@ant-design/icons';
 import Logo from '@public/icons/logo.svg?react';
@@ -11,10 +10,11 @@ import ExitIcon from '@public/icons/exit.svg?react';
 import styles from './sidebar.module.less'
 
 const {Sider} = Layout;
+const { useBreakpoint } = Grid;
 
 export const SideBar = () => {
     const {collapsed, toggleCollapsed} = useSidebarContext();
-    const {width = 0} = useWindowSize();
+    const screens = useBreakpoint();
 
     return (
         <div className={styles.wrapper} data-collapsed={collapsed}>
@@ -22,25 +22,28 @@ export const SideBar = () => {
                 collapsible
                 collapsed={collapsed}
                 trigger={null}
-                collapsedWidth={width > 425 ? 64 : 0}
-                width={width > 425 ? 208 : 106}
+                collapsedWidth={screens.xs ? 0 : 64}
+                width={screens.xs ? 106 : 208}
                 className={styles.sidebar}
             >
-                <div className={styles['logo-wrapper']}>
-                    <Link to={'/'} className={styles.logo}>{collapsed ? <LogoMini/> :
-                        <Logo/>}</Link>
-                    <Link to={'/'} className={styles.mobile}><LogoMobile/></Link>
+                <div className={styles.logoWrapper}>
+                    <Link to={'/'} className={styles.logo}>
+                        {collapsed ? <LogoMini/> : <Logo/>}
+                    </Link>
+                    <Link to={'/'} className={styles.mobile}>
+                        <LogoMobile/>
+                    </Link>
                 </div>
                 <MenuList/>
             </Sider>
             <div>
                 <Divider className={styles.divider}/>
-                <Button className={styles['exit-btn']} type='link'
-                        icon={width > 425 ? <ExitIcon/> : ''}>{!collapsed && 'Выход'}</Button>
+                <Button className={styles.exitBtn} type='link'
+                        icon={screens.xs ? '' : <ExitIcon/>}>{!collapsed && 'Выход'}</Button>
             </div>
             <div className={styles.toggler}>
                 <Button type={'default'} onClick={toggleCollapsed}
-                        data-test-id={width > 425 ? 'sider-switch' : 'sider-switch-mobile'}
+                        data-test-id={screens.xs ? 'sider-switch-mobile' : 'sider-switch'}
                         icon={collapsed ? <MenuUnfoldOutlined/> : <MenuFoldOutlined/>}></Button>
             </div>
         </div>
